2529848ee7084c46a84febe2f74b75403869c18c
Zakariya Sidki <zsidki@e1r2p2.1337.ma>
2022-05-08 20:23:26 +0100

    yml file test

--- srcs/NGINX/tools/index.php
+++ srcs/NGINX/tools/index.php
@@ -12,7 +12,7 @@
     <noscript>You need to enable JavaScript to run this app.</noscript>
     <div class="embed-responsive embed-responsive-16by9">
         <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/RaXKa9_VlH0" allowfullscreen></iframe>
-      </div>
+    </div>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
 </body>
