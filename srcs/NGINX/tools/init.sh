9de0d21016f5d4b8c57d0f68f029dac7d955f75c
Zakariya Sidki <zsidki@e1r2p2.1337.ma>
2022-05-10 20:00:34 +0100

    nginx err

--- srcs/NGINX/tools/init.sh
+++ srcs/NGINX/tools/init.sh
@@ -1,6 +1,6 @@
 mkdir /etc/nginx/ssl
 openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/CN=zak.1337.ma"
-# mv nginx.conf /etc/nginx/nginx.conf
+mv nginx.conf /etc/nginx/nginx.conf
 
 
 rm -rf /etc/nginx/sites-enabled/* && \
