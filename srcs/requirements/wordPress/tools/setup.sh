2ff77f8441e3a4521663b4548c9baf67509d8841
Zakariya Sidki <zsidki@e1r9p7.1337.ma>
2022-05-18 16:00:28 +0100

    php with nginx docker compose work

--- srcs/requirements/wordPress/tools/setup.sh
+++ srcs/requirements/wordPress/tools/setup.sh
@@ -14,11 +14,16 @@ mkdir -p /var/run/php
 # echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
 # mkdir -p /var/run/php
 
-# # sed 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 0.0.0.0:9000/' -i /etc/php/7.3/fpm/pool.d/www.conf
+#sed 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 0.0.0.0:9000/' -i /etc/php/7.3/fpm/pool.d/www.conf
 
 
 # service php7.3-fpm start
 # # fi 
 # # php-fpm7.3 --nodaemonize
 
-
+cd /tmp
+apt-get install  vim -y
+apt-get install curl -y
+curl -LO https://wordpress.org/latest.tar.gz
+tar xzvf latest.tar.gz
+cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php
