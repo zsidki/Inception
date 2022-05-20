5b47a1dda936d0a4bb95b83d76662e3fd6850089
Zakariya Sidki <zsidki@e1r9p7.1337.ma>
2022-05-20 21:23:07 +0100

    mariadb

---
+++ srcs/requirements/mariaDB/tools/setup.sh
@@ -0,0 +1,10 @@
+if [ ! -d "/var/lib/mysql/wordpress" ]; then 
+    service mysql start
+        mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+        mysql -u root -e "CREATE DATABASE $WORDPRESS_DB_NAME;"
+        mysql -u root -e "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO '$MYSQL_USER'@'%';"
+        mysql -u root -e "FLUSH PRIVILEGES;"
+        mysql -u $MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_DATABASE < ./tools/wp.sql 
+        mysqladmin --user=root password $MYSQL_ROOT_PASSWORD
+fi
+    mysqld_safe
--- srcs/requirements/wordPress/tools/wp-config.php
+++ srcs/requirements/wordPress/tools/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'Wordpress' );
+define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', '1234' );
+define( 'DB_USER', 'wordpress' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '1234' );
+define( 'DB_PASSWORD', 'wordpress' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mariadb' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
