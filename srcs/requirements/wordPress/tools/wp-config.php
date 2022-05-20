8c27aff9fdaa3ed350f540aacc30b7c165bc2915
Zakariya Sidki <zsidki@e1r9p7.1337.ma>
2022-05-20 17:19:06 +0100

    fix wordpress

--- srcs/requirements/wordPress/tools/wp-config.php
+++ srcs/requirements/wordPress/tools/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'WordpressDB' );
+define( 'DB_NAME', 'Wordpress' );
 
 /** MySQL database username */
 define( 'DB_USER', '1234' );
@@ -87,4 +87,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /** Sets up WordPress vars and included files. */
-require_once( ABSPATH . 'wp-settings.php' );
+require_once(ABSPATH . 'wp-settings.php');
