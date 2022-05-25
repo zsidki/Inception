7b6a3d3654cb4643bab394d8158a01036b46d020
Zakariya Sidki <zsidki@e1r6p5.1337.ma>
2022-05-25 13:58:26 +0100

    add some scripts

---
+++ clean docker.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#docker cleanup
+function cleandocker() {
+docker stop $(docker ps -qa); docker rm $(docker ps -qa); docker rmi -f $(docker images -qa); docker volume rm $(docker volume ls -q); docker network rm $(docker network ls -q) 2> dev/null
+}
